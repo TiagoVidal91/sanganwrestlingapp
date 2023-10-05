034be3fa38e7d8bfb9cfd37ccec758ed7208b925
Clara Correia <83609927+clara-a-correia@users.noreply.github.com>
2023-10-05 19:02:56 +0100

    Add controller, service and repo for sangen brand

--- src/main/java/com/bearAndPupperCo/sangenWrestlingApp/SangenWrestlingAppApplication.java
+++ src/main/java/com/bearAndPupperCo/sangenWrestlingApp/SangenWrestlingAppApplication.java
@@ -2,8 +2,9 @@ package com.bearAndPupperCo.sangenWrestlingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SangenWrestlingAppApplication {
 
 	public static void main(String[] args) {
