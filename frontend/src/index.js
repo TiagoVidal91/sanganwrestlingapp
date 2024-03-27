3cdf17d9f0e7a976cd4b374d0f912ee13bbc3bde
Clara Correia <83609927+clara-a-correia@users.noreply.github.com>
2024-03-27 16:18:45 +0000

    Issue 23
    Front-end provider implementation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from "@auth0/auth0-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Auth0Provider
+      domain="dev-hjyor4m7jens4wru.eu.auth0.com"
+      clientId="FWpXDOcJ3T6OgCLYRHhzrBWDKQKrc9IR"
+      redirectUri={window.location.origin}
+    >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
 );
 
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 const LandingPage = () => {
+    const { loginWithRedirect } = useAuth0();
     console.log("landingPage");
+    
     return (
         <>
         <h1>Hi!!! :3</h1>
+        <div>
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+        </div>
         </>
+        
     )
 }
 
