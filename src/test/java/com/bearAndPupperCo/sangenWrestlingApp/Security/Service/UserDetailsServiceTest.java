6e583394071595f4d7a38883d29c9583aeadd79e
Tiago Vidal <tiagovidal1991@hotmail.com>
2024-04-15 16:35:09 +0100

    Task 42 - Unit Tests [Auth Service]

--- src/test/java/com/bearAndPupperCo/sangenWrestlingApp/Security/Service/UserDetailsServiceTest.java
+++ src/test/java/com/bearAndPupperCo/sangenWrestlingApp/Security/Service/UserDetailsServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
-class UserDetailsServiceImplTest {
+class UserDetailsServiceTest {
 
     @InjectMocks
     UserDetailsServiceImpl userDetailsService;
